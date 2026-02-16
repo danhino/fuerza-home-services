2458aebd32c35b3eba24739176e7c1b8b69f7716
1inamill <dan.hinojosa@gmail.com>
2026-02-16 11:02:11 -0600

    initial mobile app setup

--- mobile/src/constants/Config.ts
+++
@@ -1,5 +0,0 @@
-// Replace localhost with your machine's LAN IP if running on physical device
-const BASE_URL = 'http://10.0.0.126:3000';
-
-export const API_URL = `${BASE_URL}/api`;
-export const SOCKET_URL = BASE_URL;
